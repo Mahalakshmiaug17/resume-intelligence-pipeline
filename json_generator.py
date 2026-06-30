9a205e2cfe39996d904713e104b0e4c2035f9e15
Mahalakshmi <145326816+Mahalakshmiaug17@users.noreply.github.com>
2026-06-30 20:07:00 +0530

    Update json_generator.py

--- json_generator.py
+++ json_generator.py
@@ -30,6 +30,10 @@ class JSONGenerator:
 
         for field in self.merged_data:
 
+            # Ignore CSV ID
+            if field == "id":
+                continue
+
             score = self.confidence.get(field, 0.0)
 
             final_json[field] = {
@@ -40,7 +44,6 @@ class JSONGenerator:
 
             overall_confidence += score * weights.get(field, 0)
 
-        # Add overall confidence
         final_json["overall_confidence"] = round(overall_confidence, 2)
 
         return final_json
