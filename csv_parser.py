4416e923fc9c468b9269c7e2f99c90991a0e18dc
Mahalakshmi <145326816+Mahalakshmiaug17@users.noreply.github.com>
2026-07-01 11:37:45 +0530

    Add files via upload

---
+++ csv_parser.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+class CSVParser:
+    """
+    Parses candidate information from a recruiter CSV file.
+    """
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def parse(self):
+
+        try:
+            df = pd.read_csv(self.file_path)
+
+            candidates = df.to_dict(orient="records")
+
+            return candidates
+
+        except Exception as e:
+            print(f"Error reading CSV: {e}")
+            return []
